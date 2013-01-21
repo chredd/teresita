439d8920142ef546eafa580cd52a470c26ad33e8
Christoffer Larsson <christoffer.larsson@aftonbladet.se>
2013-01-21 20:47:39 +0100

    Major update

--- lib/rebus.php
+++ lib/rebus.php
@@ -33,18 +33,21 @@ class Rebus extends stdClass
 
 		foreach ($data as $word => $node) {
 			# code..
+			$image = false;
 			?>
-			<div class="span3 item">
-					<img class="image" src="<?php echo $node->image ?>" alt="">
+			<div class="span4 item">
+					<?php if(!empty($node->image) ): $image = true; ?>
+					<img class="image" src="<?php echo $node->image ?>" alt="" title="Hm.. vad kan det här vara?">
+					<?php endif; ?>
 					<?php if( $node->text ): ?>
-					<span class="text"><?php echo $node->text ?></span>
+					<span class="text <?php echo !$image ? "no-image" : "" ?>"><?php echo $node->text ?></span>
 					<?php endif; ?>
 			</div>
 			
 			<?php
 
 			$count++;
-			if($count%4 == 0) echo '</div><div class="row-fluid">';
+			if($count%3 == 0) echo '</div><div class="row-fluid">';
 		}
 
 		echo '</div>';
--- templates/layout.php
+++ templates/layout.php
@@ -12,12 +12,33 @@
     <div class="container-fluid" id="main">
       <div class="row-fluid">
           <!--Body content-->
-          <h1>Hello there!</h1>
-          <?php 
-          if(isset($template) && file_exists( realpath(".") . '/' . $config->templates_path . '/' . $template .'.php' )){
-            include $template . '.php'; 
-            }
-          ?>            
+          <h1>Hej Amalia Terese!</h1>
+          <p>Vad roligt att du hittade hit. Först och främst ett stort, <strong>STORT grattis på din stora dag!</strong><br>
+          Jag önskar verkligen att jag kunde ha varit hemma hos dig idag och uppvaktat dig på riktigt.
+          Nåväl, det blir inte alltid som man vill. Här kommer ändå en liten present till dig.</p>
+
+          <p>Först blir det dock lite uppvärmning. Du kommer väl ihåg hur rebusar fungerar? <br>
+          <strong>Vi inleder med en enkel:</strong></p>
+          
+           
+          <div class="rebus-container">
+            <?php print_r( Rebus::renderRebus('json/simple.json') );  ?>
+          </div>
+
+          <p>Väldigt lätt va? Rätt svar var alltså: Jag Älskar Dig!</p>
+          <p>Detta var dock så klart inte den riktiga utmaning. Den kommer istället här!<br>
+            Om problem uppstår för du givetvis kontakta supporten på 070-370 69 29 (29,90:- per minut).</p>
+
+          <h2>Den riktiga utmaningen:</h2>
+
+
+            <div class="rebus-container">
+            <?php 
+            if(isset($template) && file_exists( realpath(".") . '/' . $config->templates_path . '/' . $template .'.php' )){
+              include $template . '.php'; 
+              }
+            ?>
+            </div>            
           </div>
 
           
